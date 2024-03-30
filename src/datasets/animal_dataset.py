049224f614a2879757de95518c30efffb2f7b384
lfx <lordfighterx@gmail.com>
2024-03-30 18:09:53 +0100

    feat: update read image with PIL

--- src/datasets/animal_dataset.py
+++ src/datasets/animal_dataset.py
@@ -1,13 +1,17 @@
 import os
 import pandas as pd
 
-from torchvision.io import read_image
+from PIL import Image
 from torch.utils.data import Dataset
 
 
 class AnimalDataset(Dataset):
     def __init__(
-        self, annotations_file, img_dir, transform=None, target_transform=None
+        self,
+        annotations_file,
+        img_dir,
+        transform=None,
+        target_transform=None,
     ):
         self.img_labels = pd.read_csv(annotations_file)
         self.img_dir = img_dir
@@ -16,7 +20,11 @@ class AnimalDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
+
+        with open(img_path, "rb") as f:
+            image = Image.open(f)
+            image = image.convert("RGB")
+
         label = self.img_labels.iloc[idx, 1]
 
         if self.transform:
