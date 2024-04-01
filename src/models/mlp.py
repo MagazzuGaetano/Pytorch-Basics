cd6254b99d971e1b943c710b0217b67d2170d916
lfx <lordfighterx@gmail.com>
2024-04-01 20:20:58 +0200

    feat: update mlp model

--- src/models/mlp.py
+++ src/models/mlp.py
@@ -1,4 +1,5 @@
 from torch import nn
+from torchsummary import summary
 
 
 class MLP(nn.Module):
@@ -7,12 +8,15 @@ class MLP(nn.Module):
         self.flatten = nn.Flatten()
         self.relu = nn.ReLU()
         self.fc1 = nn.Linear(224 * 224 * 3, 512)
-        self.fc2 = nn.Linear(512, 128)
-        self.fc3 = nn.Linear(128, 10)
+        self.fc2 = nn.Linear(512, 10)
 
     def forward(self, x):
         x = self.flatten(x)
         x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
-        logits = self.fc3(x)
+        logits = self.fc2(x)
         return logits
+
+
+if __name__ == "__main__":
+    model = MLP()
+    summary(model, (3, 224, 224), 16)
